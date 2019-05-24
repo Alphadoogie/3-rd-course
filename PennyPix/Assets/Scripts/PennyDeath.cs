9a29afa73f65448124e4ebb1fd316cb1b7bda992
Artem ZavadiĆ <artzav630@gmail.com>
2019-05-24 22:26:52 +0300

    1-st Posted KPI project

---
+++ PennyPix/Assets/Scripts/PennyDeath.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PennyDeath : MonoBehaviour
+{
+    public new ParticleSystem particleSystem;
+    private float posX, posY, posZ;
+    public GameObject Player;
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            posX = Player.transform.position.x;
+            posY = Player.transform.position.y;
+            posZ = Player.transform.position.z;
+            particleSystem.transform.position = new Vector3(posX, posY, posZ);
+            particleSystem.Play();
+            Destroy(Player);
+            StartCoroutine(Timer());
+        }
+    }
+    IEnumerator Timer()
+    {
+        yield return new WaitForSeconds(2f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
